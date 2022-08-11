c6ee6e13579c8b0b46c285b05fd39b34068f45da
Вячеслав Евстигнеев <mail@evstigneew.com>
2022-08-11 14:32:16 +0000

    update

--- src/Handbook.php
+++ src/Handbook.php
@@ -5,6 +5,7 @@ namespace OrangeCatHouse\PlanfixPhpApi;
 class Handbook extends Planfix {
 
 	protected $api;
+	protected $items = [];
 
 	public function __construct($api) {
 		$this->api = $api;
@@ -19,11 +20,44 @@ class Handbook extends Planfix {
 	}
 
 	public function getStructure($handbook_id) {
-		return $this->api->setMethod('handbook.getStructure')->setField('handbook', ['id' => $handbook_id])->send();
+		$request = $this->api->setMethod('handbook.getStructure')->setField('handbook', ['id' => $handbook_id])->send();
+		ddd($request);
+		$data = $request['data'];
+		if(isset($data['records']['record'])) {
+				$this->checkRecord($handbook_id, $data['records']['record']);
+		} elseif(isset($data['records']['records'])) {
+				foreach ($data['records']['records'] as $new_record) {
+					$this->checkRecord($handbook_id, $new_record);
+				}
+		}
+
+		return $this->items;
 	}
 
-	public function getRecords($handbook_id) {
-		return $this->api->setMethod('handbook.getRecords')->setField('handbook', ['id' => $handbook_id])->send();
+	public function getRecords($handbook_id, $key = NULL) {
+		if($key) {
+			$this->api->setParentKey($key);
+		}
+		$request = $this->api->setMethod('handbook.getRecords')->setField('handbook', ['id' => $handbook_id])->send();
+		$data = $request['data'];
+		if(isset($data['records']['record'])) {
+				$this->checkRecord($handbook_id, $data['records']['record']);
+		} elseif(isset($data['records']['records'])) {
+				foreach ($data['records']['records'] as $new_record) {
+					$this->checkRecord($handbook_id, $new_record);
+				}
+		}
+
+		return $this->items;
+	}
+
+	public function checkRecord($handbook_id, $record) {
+		if($record['isGroup']) {
+			$key = $record['key'];
+			$this->getRecords($handbook_id, $key);
+		} else {
+			$this->items[$record['key']] = $record;
+		}
 	}
 
 	public function getRecord($handbook_id, $key_id) {
@@ -52,6 +86,29 @@ class Handbook extends Planfix {
 		}
 	}
 
+	// public function checkRecord($pf, $record) {
+	//     if($record['isGroup']) {
+	//         $key = $record['key'];
+	//         getRecords($pf, $key);
+	//     } else {
+	//         $GLOBALS['records'][$record['key']] = $record;
+	//     }
+	// }
+	//
+	// public function getRecords($key = NULL) {
+	//     if($key) {
+	//         $pf->setParentKey($key);
+	//     }
+	//     $request = $this->pf->send();
+	//     $new_records = $request['data'];
+	//     if(isset($new_records['records']['record'])) {
+	//         checkRecord($pf, $new_records['records']['record']);
+	//     } elseif(isset($new_records['records']['records'])) {
+	//         foreach ($new_records['records']['records'] as $new_record) {
+	//             checkRecord($pf, $new_record);
+	//         }
+	//     }
+	// }
 // 	public function WgetRecordMulti($handbook_id, $keys_id) {
 // 		$this->api->setMethod('handbook.getRecordsMulti')->setField('handbook', ['id' => $handbook_id]);
 // 		foreach($keys_id as $key) {
